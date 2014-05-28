5b757a8de1b83da21ced592679365404561c8ee0
ajfisher <ajfisher.td@gmail.com>
2014-05-29 09:19:14 +1000

    new sequence demo

--- core_test/sequencing.h
+++ core_test/sequencing.h
@@ -7,8 +7,8 @@ uint32_t const sequence_table[][7] PROGMEM =
   //DECO BUILD RED
   
       {2000, SEQ_FAN_FADE_ON, 0, 100, 100, 100, 50},
-      {6000, SEQ_FAN_FADE_OFF, 0, 100, 100, 100, 17},
-      {10000, SEQ_FAN_STREAK_UP, 0, 100, 0, 0, 1},
+      /**{6000, SEQ_FAN_FADE_OFF, 0, 100, 100, 100, 17},
+      {10000, SEQ_FAN_STREAK_UP, 0, 100, 0, 0, 1},**/
 /**
     {2000, SEQ_FAN_UNBUILD_DOWN, 0, 100, 100, 100, 3},
     {6000, SEQ_FAN_BUILD_UP, 0, 100, 100, 100, 1},
